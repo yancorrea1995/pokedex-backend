bb432d9e9edaa32087eb0a1dff0436ae1a3ebd7b
Yan <yancorrea1995@gmail.com>
2019-01-15 17:39:23 -0300

    adicionando o metodo de insercao no BD

--- public/json.php
+++ public/json.php
@@ -2,8 +2,8 @@
 
 $json = file_get_contents('https://pokeapi.co/api/v2/pokemon/pikachu/');
 $obj = json_decode($json);
-echo $obj->id;
-echo $obj->name;
+echo $obj->id +'<br/>';
+echo $obj->name +'<br/>';
 echo $obj->types[0].name;
 
 ?>
