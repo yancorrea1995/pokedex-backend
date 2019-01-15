f79928b2bfdee307c79ff62be5da50d0911daa37
Yan <yancorrea1995@gmail.com>
2019-01-15 13:58:32 -0300

    adicionando o metodo de insercao no BD

--- public/list.php
+++ public/list.php
@@ -4,14 +4,16 @@ header('Access-Control-Allow-Origin: *');
 require 'connect.php';
     
 $pokemon = [];
-$sql = "SELECT idpokemon, day FROM captured";
+$sql = "SELECT id,idpokemon,name,day FROM captured";
 
 if($result = mysqli_query($conn,$sql))
 {
   $cr = 0;
   while($row = mysqli_fetch_assoc($result))
   {
-    $pokemon[$cr]['idpokemon']    = $row['idpokemon'];
+  	$pokemon[$cr]['id']    = $row['id'];
+  	$pokemon[$cr]['pokemonid']    = $row['pokemonid'];
+    $pokemon[$cr]['name']    = $row['name'];
     $pokemon[$cr]['day'] = $row['day'];
     $cr++;
   }
