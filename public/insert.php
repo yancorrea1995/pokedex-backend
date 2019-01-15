3e8b9db935ec84dab956541e3b1dcaf2e1db6e5f
Yan <yancorrea1995@gmail.com>
2019-01-15 14:20:21 -0300

    adicionando o metodo de insercao no BD

--- public/insert.php
+++ public/insert.php
@@ -3,36 +3,18 @@
 header('Access-Control-Allow-Origin: *');
 require 'connect.php';
     
+    if(!empty($_POST))
+    {
+    	$output = '';
+		$name2 = mysqli_real_escape_string($conn,$_POST["name"]);
+		$name = 'meu nome';
+		$day = "0000-00-00";
+		$query = "INSERT INTO captured(pokemonid,name,day) VALUES (25,'$name','$day')";
 
-	$output = '';
-	$name2 = mysqli_real_escape_string($conn,$_POST["name"]);
-	$name = 2;
-	$day = "0000-00-00";
+		if(mysqli_query($conn,$query))
+		{
+			echo 'OK';	
+		}
+    }
 
-	$query = "INSERT INTO captured(pokemonid,name,day) VALUES (25,'$name','$day')";
-
-	mysqli_query($conn,$query) or die(mysqli_error($db));
-
-
-
-
-$pokemon = [];
-$sql = "SELECT idpokemon, day FROM captured";
-
-if($result = mysqli_query($conn,$sql))
-{
-  $cr = 0;
-  while($row = mysqli_fetch_assoc($result))
-  {
-    $pokemon[$cr]['idpokemon']    = $row['idpokemon'];
-    $pokemon[$cr]['day'] = $row['day'];
-    $cr++;
-  }
-    
-  echo json_encode($pokemon);
-}
-else
-{
-  http_response_code(404);
-}
 ?>
