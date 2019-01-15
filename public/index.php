697101d25435b12fc401d93f9052faf5b938794d
Yan <yancorrea1995@gmail.com>
2019-01-14 22:38:13 -0300

    criando consultas BD

--- public/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-mysqli_connect("us-cdbr-iron-east-01.cleardb.net:3306", "be1ac295a270bf", "8b76acd3", "heroku_1f10eb5821ace69") or die(mysqli_error());
-
-echo "Conectado ao MySQL<br />";
-
-?>
