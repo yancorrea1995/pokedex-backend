237fb88e1b5a9ad112b87200c84f0aeac6c81adb
Yan <yancorrea1995@gmail.com>
2019-01-14 22:41:26 -0300

    criando consultas BD

--- public/connect.php
+++ public/connect.php
@@ -1,20 +1,26 @@
 <?php
 
-$conn = mysqli_connect("us-cdbr-iron-east-01.cleardb.net:3306", "be1ac295a270bf", "8b76acd3", "heroku_1f10eb5821ace69");
+// db credentials
+define('DB_HOST', 'us-cdbr-iron-east-01.cleardb.net:3306');
+define('DB_USER', 'be1ac295a270bf');
+define('DB_PASS', '8b76acd3');
+define('DB_NAME', 'heroku_1f10eb5821ace69');
 
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+// Connect with the database.
+function connect()
+{
+  $connect = mysqli_connect(DB_HOST ,DB_USER ,DB_PASS ,DB_NAME);
+
+  if (mysqli_connect_errno($connect)) {
+    die("Failed to connect:" . mysqli_connect_error());
+  }
+
+  mysqli_set_charset($connect, "utf8");
+
+  return $connect;
 }
 
-echo "Conectado ao MySQL<br />";
+$con = connect();
 
-$sql = "INSERT INTO captured (idpokemon, day)
-VALUES ('John', 'Doe', 'john@example.com')";
-
-if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
 
 ?>
