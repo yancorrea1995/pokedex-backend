adedc72e479764ef881a311f791e861b4c9af072
Yan <yancorrea1995@gmail.com>
2019-01-15 18:10:00 -0300

    adicionando o metodo de insercao no BD

--- public/insert.php
+++ public/insert.php
@@ -5,15 +5,23 @@ require 'connect.php';
     
     if(!empty($_POST))
     {
+		//Load vars
 		$name = mysqli_real_escape_string($conn,$_POST["name"]);
 		$day = mysqli_real_escape_string($conn,$_POST["day"]);
 
-		
+
+		//String To Lower
+		$name = strtolower($name);
+
+		//Get pokemon id
+		$json = file_get_contents("https://pokeapi.co/api/v2/pokemon/'$name'/");
+		$obj = json_decode($json);
+		$pokemonid = $obj->id;
 
 
 
 
-		$query = "INSERT INTO captured(pokemonid,name,day) VALUES ('25','$name','$day')";
+		$query = "INSERT INTO captured(pokemonid,name,day) VALUES ('$pokemonid','$name','$day')";
 
 		if(!mysqli_query($conn,$query))
 		{
--- public/json.php
+++ public/json.php
@@ -2,7 +2,6 @@
 
 $json = file_get_contents('https://pokeapi.co/api/v2/pokemon/pikachu/');
 $obj = json_decode($json);
-echo $obj->id +'<br/>';
-echo $obj->name;
+echo $obj->id;
 
 ?>
