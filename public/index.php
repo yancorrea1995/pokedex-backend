f0f67dabcde47850a6bef133441e09f056cbca42
Yan <yancorrea1995@gmail.com>
2019-01-14 21:04:02 -0300

    criando conexao com BD

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+mysqli_connect("kavfu5f7pido12mr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306", "ibdag7lookr1rw9m", "f842guwbrd95et5d", "n68s0e0zvaxksdzy") or die("Error 0101"+mysqli_error());
+
 echo "Connected to MySQL<br />";
 
 ?>
