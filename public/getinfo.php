c45e7e58872deebf38bf273596ff1f1fec6d48e1
Yan <yancorrea1995@gmail.com>
2019-01-16 18:28:40 -0300

    add getinfo method

--- public/getinfo.php
+++ public/getinfo.php
@@ -14,9 +14,23 @@
 	$pokemon = [];
 
 	$json = file_get_contents('https://pokeapi.co/api/v2/pokemon-species/'.$id.'/');
-	$obj = json_decode($json);
+	$obj = json_decode($json,true);
+
+
+	$c = count($obj['flavor_text_entries']);
+	$languageIndex = 0;
+
+	//search for english description
+	for ($i=0; $i < $c; $i++) { 
+		if($obj['flavor_text_entries'][$i]['language']['name'] == 'en')
+		{
+			$languageIndex = $i;
+			$i = $c; //to exit for
+		}
+	}
+
 	
-	$description = $obj->flavor_text_entries[2]->flavor_text;
+	$description = $obj['flavor_text_entries'][$languageIndex]['flavor_text'];
 	$pokemon[0]['description'] = $description;
 	//$pokemon[0]['nome']='Teste';
 
