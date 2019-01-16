561f2a6e8da0f6d0af03d95f1e8aed1f855e1ab2
Yan <yancorrea1995@gmail.com>
2019-01-16 12:34:06 -0300

    bd remove method added

--- public/json.php
+++ public/json.php
@@ -1,7 +1,11 @@
 <?php
 
-$json = file_get_contents('https://pokeapi.co/api/v2/pokemon/pikachu/');
-$obj = json_decode($json);
-echo $obj->id;
+	header('Access-Control-Allow-Origin: *');
+	require 'connect.php';
+	ini_set("allow_url_fopen", 1);
+
+	$json = file_get_contents('https://pokeapi.co/api/v2/pokemon/25/');
+	$obj = json_decode($json);
+	echo $obj->moves;
 
 ?>
