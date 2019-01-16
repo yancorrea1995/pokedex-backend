7710c0f91e6639591ea673dd68e237e093a37906
Yan <yancorrea1995@gmail.com>
2019-01-16 20:55:35 -0300

    add getinfo method

--- public/getinfo.php
+++ public/getinfo.php
@@ -4,13 +4,11 @@
 	ini_set("allow_url_fopen", 1);
 
 	$id = $_GET['id'];
-
 	$pokemon = [];
 
+	//JSON 1
 	$json = file_get_contents('https://pokeapi.co/api/v2/pokemon-species/'.$id.'/');
 	$obj = json_decode($json,true);
-
-
 	$c = count($obj['flavor_text_entries']);
 	$languageIndex = 0;
 
@@ -24,15 +22,16 @@
 	}
 	$description = $obj['flavor_text_entries'][$languageIndex]['flavor_text'];
 
+	//JSON 2
 	$json2 = file_get_contents('https://pokeapi.co/api/v2/pokemon/'.$id.'/');
 	$obj2 = json_decode($json2,true);
 	$height = $obj2['height'];
 	$weight = $obj2['weight'];
 
 	$d = count($obj2['types']);
-
+	$pokemon[0]['types'][0]['count'] = $d; //number of pokemons type
 	for ($j=0; $j < $d; $j++) {
-		$pokemon[0]['types'][$j]['name'] = $obj2['types'][$j]['type']['name'];
+		$pokemon[0]['types'][$j+1]['name'] = $obj2['types'][$j]['type']['name'];
 	}
 
 	
