6a9cfdaf0864d150767c63a152ee6e28c9de5d9b
Yan <yancorrea1995@gmail.com>
2019-01-16 13:52:44 -0300

    add view method

---
+++ public/view.php
@@ -0,0 +1,29 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+require 'connect.php';
+    
+$id = mysqli_real_escape_string($conn,$_POST["id"]);
+
+$pokemon = [];
+$sql = "SELECT id,pokemonid,name,day FROM captured WHERE id='$id'";
+
+if($result = mysqli_query($conn,$sql))
+{
+  $cr = 0;
+  while($row = mysqli_fetch_assoc($result))
+  {
+  	$pokemon[$cr]['id'] = $row['id'];
+  	$pokemon[$cr]['pokemonid'] = $row['pokemonid'];
+    $pokemon[$cr]['name'] = $row['name'];
+    $pokemon[$cr]['day'] = $row['day'];
+    $cr++;
+  }
+  
+  echo json_encode($pokemon);
+}
+else
+{
+  http_response_code(404);
+}
+?>
