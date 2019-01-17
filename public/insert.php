3e951cba14c6bb77731eab2cfc3df05575bbeff4
Yan <yancorrea1995@gmail.com>
2019-01-17 12:40:38 -0300

    add getinfo method

--- public/insert.php
+++ public/insert.php
@@ -9,11 +9,23 @@
     {
 		$name = mysqli_real_escape_string($conn,$_POST["name"]);
 		$day = mysqli_real_escape_string($conn,$_POST["day"]);
+		$move0 = mysqli_real_escape_string($conn,$_POST['move0']);
+		$move1 = mysqli_real_escape_string($conn,$_POST['move1']);
+		$move2 = mysqli_real_escape_string($conn,$_POST['move2']);
+		$move3 = mysqli_real_escape_string($conn,$_POST['move3']);
+		$move4 = mysqli_real_escape_string($conn,$_POST['move4']);
+		$move5 = mysqli_real_escape_string($conn,$_POST['move5']);
+		$move6 = mysqli_real_escape_string($conn,$_POST['move6']);
+		$move7 = mysqli_real_escape_string($conn,$_POST['move7']);
+		$move8 = mysqli_real_escape_string($conn,$_POST['move8']);
+		$move9 = mysqli_real_escape_string($conn,$_POST['move9']);
+		$move10 = mysqli_real_escape_string($conn,$_POST['move10']);
+		$move11 = mysqli_real_escape_string($conn,$_POST['move11']);
+		$move12 = mysqli_real_escape_string($conn,$_POST['move12']);
+		$move13 = mysqli_real_escape_string($conn,$_POST['move13']);
+		$move14 = mysqli_real_escape_string($conn,$_POST['move14']);
+		$move15 = mysqli_real_escape_string($conn,$_POST['move15']);
 
-		$move = [];
-		for ($i=0; $i < 16; $i++) { 
-			$move[i] = mysqli_real_escape_string($conn,$_POST['move'.$i]);
-		}
 
 		//Convert to lowercase
 		$name=strtolower($name);
@@ -51,7 +63,7 @@
 		}
 
 		for ($i=0; $i < 16 ; $i++) {
-			$query2 = "INSERT INTO moves(capturedid,move) VALUES ('$lastID','$move[$i]')";
+			$query2 = "INSERT INTO moves(capturedid,move) VALUES ('$lastID','$move.$i')";
 
 			if(!mysqli_query($conn,$query2))
 				console.log("Erro Insert BD");	
